caca2f7d6239a23a31bb5ed63c537cce76de56ff
Luis Philipe <luizsale10@gmail.com>
2017-10-10 15:16:40 -0300

    Create offer with assets in SwapyExchange

--- contracts/protocol/SwapyExchange.sol
+++ contracts/protocol/SwapyExchange.sol
@@ -1,19 +1,26 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.14;
 
 import './InvestmentOffer.sol';
+import './InvestmentAsset.sol';
 
 contract SwapyExchange {
 
   // Protocol version 
   string constant public VERSION = "1.0.0";  
 
-  event Offers(address from, string protocolVersion, address offerAddress, uint256 paybackMonths, uint256 grossReturn);
+  event Offers(address _from, string _protocolVersion, address _offerAddress, uint256 _paybackMonths, uint256 _grossReturn, address[] _assets);
 
-  function createOffer(uint256 _paybackMonths, uint256 _grossReturn) 
+
+  // Creates a new investment offer
+  function createOffer(uint256 _paybackMonths, uint256 _grossReturn, uint256[] _assets) 
     returns(bool) 
   {
     address newOffer = address(new InvestmentOffer(msg.sender, VERSION, _paybackMonths, _grossReturn));
-    Offers(msg.sender, VERSION, newOffer, _paybackMonths, _grossReturn);    
+    address[] newAssets;
+    for (uint index = 0; index < _assets.length; index++) {
+      newAssets.push(address(new InvestmentAsset(msg.sender, VERSION, newOffer)));
+    }
+    Offers(msg.sender, VERSION, newOffer, _paybackMonths, _grossReturn, newAssets);    
     return true;
   }
         
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -14,6 +14,7 @@ const investor = process.env.WALLET_ADDRESS;
 const payback = 24;
 const grossReturn = 5;
 const assetValue = 10;
+const assets = [10,10,10,10,10];
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(SwapyExchange)
@@ -26,34 +27,27 @@ module.exports = function(deployer, network, accounts) {
             if(!err){
               console.log('Offer created...');
               console.log(response.args);
-              let offerAddress = response.args.offerAddress;
-              let offer = InvestmentOffer.at(offerAddress);
-              offer.Assets().watch((err,response) => {
+              let offerAddress = response.args._offerAddress;
+              let deployedAssets = response.args._assets;
+              let assetAddress = deployedAssets[0];
+              let asset = InvestmentAsset.at(assetAddress);
+              let agreeInvestment = asset.Agreements().watch((err,response) => {
                 if(!err){
-                  console.log('Asset created...');
+                  console.log('Investment Agreed...');
                   console.log(response.args);
-                  let assetAddress = response.args.assetAddress;
-                  let asset = InvestmentAsset.at(assetAddress);
-                  let agreeInvestment = asset.Agreements().watch((err,response) => {
+                  asset.Transferred().watch((err,response) => {
                     if(!err){
-                      console.log('Investment Agreed...');
+                      console.log('Asset invested...');
                       console.log(response.args);
-                      asset.Transferred().watch((err,response) => {
-                        if(!err){
-                          console.log('Asset invested...');
-                          console.log(response.args);
-                        }
-                      });
-                      asset.transferFunds(terms,{value: assetValue});
                     }
                   });
-                  asset.agreeInvestment(investor, terms, assetValue);
+                  asset.transferFunds(terms,{value: assetValue});
                 }
               });
-              offer.createAsset();
+              asset.agreeInvestment(investor, terms, assetValue);
             }
           });
-          protocol.createOffer(payback, grossReturn);  
+          protocol.createOffer(payback, grossReturn, assets);  
         });
     });
 };
