c2efda4e23b821c00c525319976cc5ed9dd1601b
Luis Philipe Fidelis <luizsale10@gmail.com>
2017-10-26 20:42:35 -0200

    create offer unit test (incomplete)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,5 @@
 let SwapyExchange   = artifacts.require("./SwapyExchange.sol");
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(SwapyExchange)
-    .then(() => {
-      SwapyExchange
-        .deployed()
-        .then(console.log);
-    });
+  deployer.deploy(SwapyExchange);
 };
--- test/SwapyExchange.js
+++ test/SwapyExchange.js
@@ -1,12 +1,44 @@
 const SwapyExchange = artifacts.require("./SwapyExchange.sol");
+
 const currentVersion = "1.0.0";
+const agreementTerms = "222222";
+const investor = process.env.WALLET_ADDRESS;
+const payback = 24;
+const grossReturn = 5;
+const assetValue = 10;
+const assets = [10,10,10,10,10];
+const currency = "USD";
+const offerFixedValue = 50;
+const offerTerms = "111111";
+
+// Example of an event uuid 
+const eventId = 'f6e6b40a-adea-11e7-abc4-cec278b6b50a';
 // ... more code
 contract('SwapyExchange', accounts => {
- 
-  it("should has a version", async function() {
-    let protocol = await SwapyExchange.deployed();
+  let protocol = null;
+  it("should has a version", async () => {
+    protocol = await SwapyExchange.deployed();
     let version = await protocol.VERSION.call();
     assert.equal(version, currentVersion, "the protocol is not versioned")
   });
+
+  it("should create an investment offer", async () => {
+    protocol.createOffer(
+        eventId,
+        payback,
+        grossReturn,
+        currency,
+        offerFixedValue,
+        offerTerms,
+        assets
+    ).then(() => {
+        let event = protocol.Offers().watch((err,response) =>{
+            assert.equal(err, null, "An error ocurred")
+            console.log(response);
+        });     
+    });
+  });
+
+  
 
 })
