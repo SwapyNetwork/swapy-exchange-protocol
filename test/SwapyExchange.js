f1a8f92282ee2a537b3874f6896e81958ec8a46e
Luis Philipe <luizsale10@gmail.com>
2017-10-31 18:01:50 -0200

    test simple flo

--- test/SwapyExchange.js
+++ test/SwapyExchange.js
@@ -1,6 +1,7 @@
 const should = require('chai')
     .use(require('chai-as-promised'))
     .should()
+const expect = require('chai').expect;
 
 const SwapyExchange = artifacts.require("./SwapyExchange.sol");
 const InvestmentAsset = artifacts.require("./investment/InvestmentAsset.sol");
@@ -11,7 +12,7 @@ const investor = process.env.WALLET_ADDRESS;
 const payback = 24;
 const grossReturn = 5;
 const assetValue = 10;
-const assets = [10];
+const assets = [10,10];
 let assetAddress = [];
 const currency = "USD";
 const offerFixedValue = 10;
@@ -24,56 +25,79 @@ contract('SwapyExchange', accounts => {
   
     let protocol = null;
 
-  it("should has a version", async () => {
-    protocol = await SwapyExchange.deployed();
-    let version = await protocol.VERSION.call();
-    assert.equal(version, currentVersion, "the protocol is not versioned")
-  })
+    it("should has a version", async () => {
+        protocol = await SwapyExchange.deployed();
+        let version = await protocol.VERSION.call();
+        assert.equal(version, currentVersion, "the protocol is not versioned")
+    })
 
-  it("should create an investment offer and log it", done => {
-    protocol.Offers({_id: eventId}).watch((err,log) => {
-        // @todo treat event
-        done();
-    });
-    const transaction = protocol.createOffer(
-        eventId,
-        payback,
-        grossReturn,
-        currency,
-        offerFixedValue,
-        offerTerms,
-        assets
-    ).then(transaction => {
-        should.exist(transaction.tx);
-    });
-  })
+    it("should create an investment offer and log it", done => {
+        protocol.Offers({_id: eventId}).watch((err,log) => {
+            const event = log.args;
+            expect(event).to.include.all.keys([
+                '_id',
+                '_from',
+                '_protocolVersion',
+                '_offerAddress',
+                '_assets'
+            ]);
+            assetAddress = event._assets;
+            done();        
+        });
+        const transaction = protocol.createOffer(
+            eventId,
+            payback,
+            grossReturn,
+            currency,
+            offerFixedValue,
+            offerTerms,
+            assets
+        ).then(transaction => {
+            should.exist(transaction.tx);
+        });
+    })
 
-  it('should add an investment', async () => {
-    let assetContract = await InvestmentAsset.at(assetAddress[0]);
-    const investment = await assetContract.invest(eventId, agreementTerms)
-        .then(async result => {
-            should.exist(result.tx)
-            return true;
-        }, error => {
-            console.log(error);
-        })
-        .then(async () => {
-            const filter = await assetContract.Transferred();
-            const event = await filter.get((err,result) => {
-                console.log(err);
-                console.log(result);
+    it('should add an investment', done => {
+        InvestmentAsset.at(assetAddress[0]).then(assetContract => {
+            assetContract.Transferred({_id: eventId}).watch((err,log) => {
+                const event = log.args;
+                expect(event).to.include.all.keys([
+                    '_id',
+                    '_from',
+                    '_to',
+                    '_value',
+                ]);
+                assert.equal(event._value, assetValue, "The invested value must be equal the sent value");
+                done();
+            });
+            assetContract.invest(eventId, agreementTerms, {value: assetValue})
+                .then(transaction => {
+                    should.exist(transaction.tx)
+                }, error => {
+                    console.log(error);
             });
         });
-  })
+    })
 
-  it('should log when add investment', async () => {
-    let assetContract = await InvestmentAsset.at(assetAddress[0]);
-    let investment = await assetContract.Transferred()
-    await  investment.get((err, logs) => {
-        return logs.forEach(logs => console.log(log.args))
-    });
-  })
-
-  
-
+    it('should accept the pending investment', done => {
+        InvestmentAsset.at(assetAddress[0]).then(assetContract => {
+            assetContract.Withdrawal({_id: eventId}).watch((err,log) => {
+                const event = log.args;
+                expect(event).to.include.all.keys([
+                    '_id',
+                    '_owner',
+                    '_investor',
+                    '_value',
+                    '_terms',
+                ]);
+                done();
+            });
+            assetContract.withdrawFunds(eventId, agreementTerms)
+                .then(transaction => {
+                    should.exist(transaction.tx)
+                }, error => {
+                    console.log(error);
+            });
+        });
+    })
 })
