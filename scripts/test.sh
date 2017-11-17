fc9c99a4bf374efb7c46790e7bbb0c608dc65b01
Luis Philipe <luizsale10@gmail.com>
2017-11-17 16:51:50 -0200

    remove network id from test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -30,7 +30,7 @@ start_testrpc() {
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501208,1000000000000000000000000"
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501209,1000000000000000000000000"
   )
-  testrpc --gasLimit 0xfffffffffff --network-id "${TEST_NETWORK_ID}" --port "$testrpc_port" "${accounts[@]}" > /dev/null &
+  testrpc --gasLimit 0xfffffffffff --port "$testrpc_port" "${accounts[@]}" > /dev/null &
   testrpc_pid=$!
 }
 
