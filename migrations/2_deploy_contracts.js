7f2df6093588ecd3dd209160e1c54258551cc22b
Ícaro Harry <me@icaro.io>
2017-11-30 16:16:50 -0200

    Create dummy FinId and unit tests for it

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,12 @@
 // --- Contracts
 let SwapyExchange = artifacts.require("./SwapyExchange.sol");
 let AssetLibrary = artifacts.require("../investment/AssetLibrary.sol");
+let FinId = artifacts.require("../identity/FinId.sol");
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(AssetLibrary).then(() => {  
+  deployer.deploy(AssetLibrary).then(() => {
     return deployer.deploy(SwapyExchange, AssetLibrary.address);
   })
+
+  deployer.deploy(FinId);
 };
