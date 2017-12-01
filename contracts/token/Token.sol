d8b7b5d4cea7788b12729f1b2fe38e09d270b7b3
Luis Philipe <luizsale10@gmail.com>
2017-12-01 17:00:15 -0200

    change token supply

--- contracts/token/Token.sol
+++ contracts/token/Token.sol
@@ -6,9 +6,9 @@ contract Token is StandardToken {
     
     string public constant name = "SwapyBeta";
     string public constant symbol = "SWBETA";
-    uint8 public constant decimals = 18;
+    uint8 public constant decimals = 2;
 
-    uint256 public constant INITIAL_SUPPLY = 100000000000000 * (10 ** uint256(decimals));
+    uint256 public constant INITIAL_SUPPLY = 1000000000000 * (10 ** uint256(decimals));
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
