ac0b6709a43efec2452caf2bda056174481be9c1
Luis Philipe <luizsale10@gmail.com>
2018-01-02 15:36:05 -0200

    remove Token's deploy from migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,10 +3,9 @@ let SwapyExchange = artifacts.require("./SwapyExchange.sol");
 let AssetLibrary = artifacts.require("./investment/AssetLibrary.sol");
 let Token = artifacts.require("./token/Token.sol");
 
+
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(AssetLibrary).then(() => {  
-    return deployer.deploy(Token).then(() => {
-      return deployer.deploy(SwapyExchange, AssetLibrary.address, Token.address);
-    })
+    return deployer.deploy(SwapyExchange, AssetLibrary.address, process.env.TOKEN_ADDRESS);
   })
 };
