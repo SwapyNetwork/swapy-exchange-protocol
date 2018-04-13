71093751bb82c2f1b58f90adf5b645f321070fdc
Luis Philipe Fidelis <luizsale10@gmail.com>
2018-04-13 15:27:13 -0300

    downgrade truffle to 4.1.4 and fix test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,15 @@ let AssetLibrary = artifacts.require("./investment/AssetLibrary.sol")
 let Token = artifacts.require("./token/Token.sol")
 
 module.exports = function(deployer, network, accounts) {
-  if(network.indexOf('test') > -1) return
-  deployer.deploy(AssetLibrary).then(() => {  
-    return deployer.deploy(SwapyExchange, AssetLibrary.address, process.env.TOKEN_ADDRESS)
-  })
+  if(network.indexOf('test') > -1) {
+    deployer.deploy(Token).then(() => {
+      return deployer.deploy(AssetLibrary).then(() => {  
+        return deployer.deploy(SwapyExchange, AssetLibrary.address, Token.address)
+      })
+    })
+  }else {
+    deployer.deploy(AssetLibrary).then(() => {  
+      return deployer.deploy(SwapyExchange, AssetLibrary.address, process.env.TOKEN_ADDRESS)
+    })
+  }
 }
--- test/TestSwapyExchange.sol
+++ test/TestSwapyExchange.sol
@@ -10,17 +10,17 @@ contract TestSwapyExchange {
     SwapyExchange protocol = SwapyExchange(DeployedAddresses.SwapyExchange());
     ThrowProxy throwProxy = new ThrowProxy(address(protocol)); 
     SwapyExchange throwableProtocol = SwapyExchange(address(throwProxy));
-    
+    uint256[] _assets;
+
     // Testing the createOffer() function
     function testUserCreateOffer() public {
-        uint256[] memory _assets;
-        _assets[0] = uint256(500);
-        _assets[1] = uint256(500);
-        _assets[2] = uint256(500);
+        _assets.push(uint256(500));
+        _assets.push(uint256(500));
+        _assets.push(uint256(500));
         bool result = protocol.createOffer(
             uint256(360),
             uint256(10),
-            "USD",
+            bytes5("USD"),
             _assets
         );
         Assert.equal(result, true, "Five assets must be created");
