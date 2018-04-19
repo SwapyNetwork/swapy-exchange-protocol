f1f2d320a1af8bb4f8d649354e3000b83b1414da
Luis Philipe <luizsale10@gmail.com>
2018-04-19 15:54:03 -0300

    investmentAsset

---
+++ test/TestInvestmentAsset.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.21;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/InvestmentAsset.sol";
+import "./helpers/ThrowProxy.sol";
+
+contract TestInvestmentAsset {
+    
+
+    
+}
