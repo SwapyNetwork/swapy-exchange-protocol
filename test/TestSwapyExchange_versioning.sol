1e39070109ddb8e6bce5ca8e1def1c4e015f06ca
Luis Philipe Fidelis <luizsale10@gmail.com>
2018-04-30 15:24:26 -0300

    test add invalid library address or duplicated version label

--- test/TestSwapyExchange_versioning.sol
+++ test/TestSwapyExchange_versioning.sol
@@ -13,11 +13,9 @@ contract TestSwapyExchange_versioning {
     // and funds this test contract with the specified amount on deployment.
     uint public initialBalance = 10 ether;
 
-    bytes msgData;
-    function() payable public {
-        msgData = msg.data;
+    function shouldThrow(bool execution) private {
+        Assert.isFalse(execution, "Should throw an exception");
     }
-
     // Testing the addLibrary() function
     function testOnlyProtocolOwnerCanAddLibrary() public {
         address lib2 = address(0x8f46cf5569aefa1acc1009290c8e043747172d45);
@@ -31,8 +29,20 @@ contract TestSwapyExchange_versioning {
         bytes8 latestVersion = protocol.latestVersion();
         address latestLib = protocol.getLibrary(latestVersion);
         bool check = (latestVersion == bytes8("3.0.0")) && (latestLib == address(lib3));
-        Assert.equal(check, true, "The latest version must be 2.0.0 and the library address of this version must be equal the lastest deployed library");
+        Assert.equal(check, true, "The latest version must be 3.0.0 and the library address of this version must be equal the lastest deployed library");
     }
 
-    
+    function testOwnerCannotAddDuplicatedVersion() public {
+        address lib3 = address(0x9f46cf5569aefa1acc1009290c8e043747172d45);
+        bool result = address(protocol).call(abi.encodeWithSignature("addLibrary(bytes8,address)", bytes8("3.0.0"), lib3));
+        shouldThrow(result);
+    }
+
+    function testOwnerCannotAddInvalidLibrary() public {
+        bool result = address(protocol).call(abi.encodeWithSignature("addLibrary(bytes8,address)", bytes8("3.0.0"), address(0)));
+        shouldThrow(result);
+    }
+
+
+
 }
